e4a5bc2bf56509b9ce2635f90f74ed94c6716b56
rompni <andresnavarrodev@gmail.com>
2023-07-20 21:04:22 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AuthenticatedSocketAdapter } from './sockets/authenticated-socket.adapter';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  //app.use(helmet())
+  //app.useWebSocketAdapter(new AuthenticatedSocketAdapter(app)); // Add our custom socket adapter.
+  await app.listen(3000);
+}
+bootstrap();
