8360a2008e06868960c1261f2ecd7cb20616cd04
rompni <andresnavarrodev@gmail.com>
2023-07-21 10:11:45 -0500

    port env

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthenticatedSocketAdapter } from './sockets/authenticated-socket.adapter';
-import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //app.use(helmet())
   //app.useWebSocketAdapter(new AuthenticatedSocketAdapter(app)); // Add our custom socket adapter.
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
